a01c62d96854b4685e6c49046464a4cf4edcefd1
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-05-22 23:23:37 +0200

    parsing feeds / feedreader package does not like es6 lambda methods ;(

---
+++ app/feedReader.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import FeedParser from 'feedparser';
+import request from 'request';
+
+var feedReader = {
+    read: read
+};
+
+function read(callback) {
+    let req = request('http://aimforsimplicity.com/feed.atom');
+    let feedparser = new FeedParser();
+    let items = [];
+
+    req.on('error', function(error) {
+        console.log(error);
+    });
+
+    req.on('response', function(res) {
+        var stream = this;
+
+        if (res.statusCode !== 200) {
+            return this.emit('error', new Error('Bad status code'));
+        }
+
+        stream.pipe(feedparser);
+    });
+
+    feedparser.on('error', function(error) {
+        console.log(error);
+    });
+
+    feedparser.on('readable', function() {
+        // This is where the action is!
+        let stream = this;
+        let meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+        let item = {};
+
+        while (item = stream.read()) {
+            items.push(item);
+        }
+    });
+
+    feedparser.on('end', function() {
+        callback(null, items);
+    });
+}
+
+module.exports = feedReader;
--- server.js
+++ server.js
@@ -2,12 +2,23 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import feedReader from './app/feedReader.js';
 
 var app = express(),
     port = 2222;
 
 // config
 app.use(bodyParser.json());
+
+// api
+
+app.get('/feeds', (req, res) => {
+    feedReader.read((error, data) => {
+        let responseData = JSON.stringify(data);
+        res.write(responseData);
+        res.end();
+    });
+});
 
 // VIEWS
 app.use(express.static('public'));
