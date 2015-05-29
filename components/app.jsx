328ac6c5cfbbc735c1b2ddb65fa039ac17e11aea
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-05-29 23:57:04 +0200

    displaying list of articles of given feed / needs improvement

--- components/app.jsx
+++ components/app.jsx
@@ -10,10 +10,16 @@ import feedsApi from './feedsApi';
 
 let mainElement = document.getElementById('main-container');
 
+let contentType = {
+    FEED: 'feed',
+    ARTICLE: 'article'
+};
+
 let Home = React.createClass({
     getInitialState() {
         return {
             feeds: [],
+            displayedArtiles: [],
             currentlyDisplayedContent: {
                 content: '',
                 title: '',
@@ -25,11 +31,26 @@ let Home = React.createClass({
     },
 
     componentDidMount() {
-        feedsApi.getFeeds((feeds) => this.setState({feeds: feeds[0].articles}));
+        feedsApi.getFeeds((feeds) => this.setState({feeds: feeds}));
+    },
+
+    _displayFeedArticles(feedAddress) {
+        console.log(feedAddress);
+        console.log(this.state.feeds);
+        let articles = _(this.state.feeds)
+        .map((feed) => {
+            if (feed.address.indexOf(feedAddress) > -1) {
+                return feed.articles;
+            }
+        })
+        .first();
+
+        console.log(articles);
+        this.setState({ displayedArtiles: articles });
     },
 
     _displayArticle(articleTitle) {
-        feedsApi.getArticle(articleTitle, (article) => this.setState({ currentlyDisplayedContent: article }));
+        feedsApi.getArticle(contentPath, (article) => this.setState({ currentlyDisplayedContent: article }));
     },
 
     _createMarkup() {
@@ -41,10 +62,10 @@ let Home = React.createClass({
     },
 
     render() {
-        let articleTitles = _.map(this.state.feeds, (article) => {
+        let feedAddresses = _.map(this.state.feeds, (feed) => {
             return (
                 <div>
-                    <span className="app-article-link" onClick={ this._displayArticle.bind(null, article.title) }>{ article.title }</span>
+                    <span className="app-article-link" onClick={ this._displayFeedArticles.bind(null, feed.address) }>{ feed.address }</span>
                 </div>
             );
         });
@@ -52,26 +73,36 @@ let Home = React.createClass({
         let date = this.state.currentlyDisplayedContent.date ? 'Date: ' + new Date(this.state.currentlyDisplayedContent.date).toString() : null;
         let author = this.state.currentlyDisplayedContent.author ? 'Author: ' + this.state.currentlyDisplayedContent.author : null;
 
+        let singleArticle = <div>
+                <div>
+                    <div><h1>{ this.state.currentlyDisplayedContent.title }</h1></div>
+                </div>
+                <div>
+                    <div>{ date }</div>
+                    <div>{ author }</div>
+                </div>
+                <div>
+                    <div dangerouslySetInnerHTML={ this._createMarkup() } />
+                </div>
+            </div>;
+
         let addButton = <AddNewFeedButton addFeedHandler={ this._addFeedHandler } />;
+
+        let artilesTitlesToDisplay = _.map(this.state.displayedArtiles, (article) => {
+            return <div className="app-article-link" onClick={ this._displayArticle.bind(null, article.title) }> { article.title } </div>;
+        });
+
+        let contentToDisplay = artilesTitlesToDisplay ? artilesTitlesToDisplay : singleArticle;
 
         return (
             <div className="Home container-fluid">
                 <div className="row">
                     <div className="menu col-md-4">
-                        { articleTitles }
+                        { feedAddresses }
                         { addButton }
                     </div>
                     <div className="feeds col-md-8">
-                        <div>
-                            <div><h1>{ this.state.currentlyDisplayedContent.title }</h1></div>
-                        </div>
-                        <div>
-                            <div>{ date }</div>
-                            <div>{ author }</div>
-                        </div>
-                        <div>
-                            <div dangerouslySetInnerHTML={ this._createMarkup() } />
-                        </div>
+                        { contentToDisplay }
                     </div>
                 </div>
             </div>
